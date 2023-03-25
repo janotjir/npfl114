628e15d6460f29c9a6398271d529abe38cede156
Jiri Janota <janotjir@fel.cvut.cz>
2023-03-25 22:36:36 +0100

    retina goes brrr

--- labs/06/svhn_competition.py
+++ labs/06/svhn_competition.py
@@ -19,7 +19,10 @@ parser.add_argument("--debug", default=False, action="store_true", help="If give
 parser.add_argument("--epochs", default=1, type=int, help="Number of epochs.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
-parser.add_argument("--iou_thr", default=0.5, type=int, help="IoU threshold for gold classes.")
+parser.add_argument("--iou_thr", default=0.5, type=float, help="IoU threshold for gold classes.")
+parser.add_argument("--cls_balancing", default=False, action="store_true", help="Focal loss class balancing")
+parser.add_argument("--alpha", default=0.25, type=float, help="Focal loss parameter")
+parser.add_argument("--gamma", default=2, type=float, help="Focal loss parameter")
 
 
 # Team members:
@@ -27,14 +30,13 @@ parser.add_argument("--iou_thr", default=0.5, type=int, help="IoU threshold for 
 # dc535248-fa6c-4987-b49f-25b6ede7c87d
 
 
-pyramid_scales = [8, 16, 32]
+pyramid_scales = [8, 16, 32, 64, 128]
 anchor_shapes = np.array([[1,1], [1,2], [2,1]])
 input_shape = [320, 320]
-A = len(pyramid_scales) * anchor_shapes.shape[0]
+A = anchor_shapes.shape[0]
 
 
-# TODO change anchor generating to match retinanet (5 pyramid scales)
-class KretinaNet(tf.keras.Model):
+class DetMuchNet(tf.keras.Model):
     def __init__(self):
         inputs = tf.keras.layers.Input(shape=[input_shape[0], input_shape[1], 3])
 
@@ -46,7 +48,12 @@ class KretinaNet(tf.keras.Model):
         box_out = self._regression_head(p)
         print(box_out.shape)
 
-        super().__init__(inputs=inputs, outputs=[cls_out, box_out])
+        outputs = {
+            "classes": cls_out,
+            "boxes": box_out
+        }
+
+        super().__init__(inputs=inputs, outputs=outputs)
 
     def _backbone(self, inputs):
         backbone = tf.keras.applications.EfficientNetV2B0(include_top=False, weights="imagenet", input_shape=(input_shape[0], input_shape[1], 3))
@@ -90,7 +97,8 @@ class KretinaNet(tf.keras.Model):
 
         for _p in p:
             _p = cls_head(_p)
-            _p = tf.reshape(_p, [args.batch_size, -1, SVHN.LABELS*A])
+            _p = tf.reshape(_p, [-1, _p.shape[1]*_p.shape[2]*A, SVHN.LABELS])
+            #_p = tf.reshape(_p, [args.batch_size, -1, SVHN.LABELS])
             cls_out.append(_p)
 
         cls_out = tf.concat(cls_out, axis=1)
@@ -109,7 +117,8 @@ class KretinaNet(tf.keras.Model):
 
         for _p in p:
             _p = box_head(_p)
-            _p = tf.reshape(_p, [args.batch_size, -1, 4*A])
+            _p = tf.reshape(_p, [-1, _p.shape[1]*_p.shape[2]*A, 4])
+            #_p = tf.reshape(_p, [args.batch_size, -1, 4])
             box_out.append(_p)
 
         box_out = tf.concat(box_out, axis=1)
@@ -125,10 +134,11 @@ def prepare_examples(img, cls, bbx):
     for scale in pyramid_scales:
         scaled_anchors = scale * anchor_shapes
         for anchor in scaled_anchors:
-            x_positions = np.arange(img.shape[0] - anchor[0] + 1)
-            y_positions = np.arange(img.shape[1] - anchor[1] + 1)
+            x_positions = np.arange(input_shape[0]/scale)
+            y_positions = np.arange(input_shape[1]/scale)
             xv, yv = np.meshgrid(x_positions, y_positions)
             upper_left = np.concatenate([xv[..., np.newaxis], yv[..., np.newaxis]], 2).reshape(-1, 2)
+            upper_left *= scale
             lower_right = upper_left + anchor[np.newaxis, :]
             anchor_coords = np.hstack([upper_left, lower_right])
             #print(anchor_coords)
@@ -136,10 +146,9 @@ def prepare_examples(img, cls, bbx):
     
     #print(anchors)
     anchor_cls, anchor_bbx = bboxes_utils.bboxes_training(anchors, cls.numpy(), bbx.numpy(), args.iou_thr)
+    anchor_cls = tf.one_hot(anchor_cls, 10)
 
     return img, anchor_cls, anchor_bbx
-
-
 
 
 def main(args: argparse.Namespace) -> None:
@@ -166,28 +175,50 @@ def main(args: argparse.Namespace) -> None:
     test = svhn.test
     
     train = train.map(lambda x: (x["image"], x["classes"], x['bboxes']))
-    train = train.map(lambda img, cls, bbx: tf.py_function(prepare_examples, inp=[img, cls, bbx], Tout=[tf.uint8, tf.int64, tf.float32]))
+    train = train.map(lambda img, cls, bbx: tf.py_function(prepare_examples, inp=[img, cls, bbx], Tout=[tf.uint8, tf.float32, tf.float32]))
+    train = train.map(lambda img, cls, bbx: (img, {"classes": cls, "boxes": bbx}))
+
+    dev = dev.map(lambda x: (x["image"], x["classes"], x['bboxes']))
+    dev = dev.map(lambda img, cls, bbx: tf.py_function(prepare_examples, inp=[img, cls, bbx], Tout=[tf.uint8, tf.float32, tf.float32]))
+    dev = dev.map(lambda img, cls, bbx: (img, {"classes": cls, "boxes": bbx}))
+
+    train = train.shuffle(args.seed)
+    train = train.batch(args.batch_size)
+    train = train.prefetch(tf.data.AUTOTUNE)
+
+    dev = dev.batch(args.batch_size)
+    dev = dev.prefetch(tf.data.AUTOTUNE)
    
-    #for img, cls, bbx in train:
-    #    print(img.shape, cls.shape, bbx.shape)
-
-    '''
-    # Load the EfficientNetV2-B0 model. It assumes the input images are
-    # represented in [0-255] range using either `tf.uint8` or `tf.float32` type.
-    backbone = tf.keras.applications.EfficientNetV2B0(include_top=False)
-
-    # Extract features of different resolution. Assuming 224x224 input images
-    # (you can set this explicitly via `input_shape` of the above constructor),
-    # the below model returns five outputs with resolution 7x7, 14x14, 28x28, 56x56, 112x112.
-    backbone = tf.keras.Model(
-        inputs=backbone.input,
-        outputs=[backbone.get_layer(layer).output for layer in [
-             "top_activation", "block5e_add", "block3b_add", "block2b_add", "block1a_project_activation"]]
-    )
-    '''
+    #for img, lbl in train:
+    #    print(img.shape, lbl["classes"].shape, lbl["boxes"].shape)
 
     # TODO: Create the model and train it
-    model = KretinaNet()
+    model = DetMuchNet()
+    model.compile(
+        optimizer=tf.optimizers.experimental.AdamW(jit_compile=False),
+        loss={
+            "classes": tf.keras.losses.BinaryFocalCrossentropy(args.cls_balancing, args.alpha, args.gamma),
+            "boxes": tf.keras.losses.Huber(),
+            #"boxes": tf.keras.losses.MeanSquaredError(),
+        },
+        metrics={
+            "classes": [tf.metrics.CategoricalAccuracy("accuracy")],
+            #"boxes": [tf.metrics.BinaryAccuracy("accuracy")],
+        },
+    )
+
+    tb_callback = tf.keras.callbacks.TensorBoard(args.logdir)
+    def save_model(epoch, logs):
+        model.save(os.path.join(args.logdir, f"ep{epoch+1}.h5"), include_optimizer=False)
+
+    with tf.device("/GPU:0"):
+        model.fit(train, epochs=args.epochs, validation_data=dev, callbacks=[tb_callback, tf.keras.callbacks.LambdaCallback(on_epoch_end=save_model)])
+
+    exit()
+
+    # TODO
+    test = test.map(lambda x: x["image"])
+    test = test.batch(args.batch_size)
 
     # Generate test set annotations, but in `args.logdir` to allow parallel execution.
     os.makedirs(args.logdir, exist_ok=True)
