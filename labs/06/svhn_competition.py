f1d0508d2e54ede6c6e13382409e6ce69a36ba33
Adam Jirkovsky <jirkoada@fel.cvut.cz>
2023-03-26 11:57:52 +0200

    Added eval

--- labs/06/svhn_competition.py
+++ labs/06/svhn_competition.py
@@ -16,15 +16,18 @@ from svhn_dataset import SVHN
 parser = argparse.ArgumentParser()
 parser.add_argument("--batch_size", default=32, type=int, help="Batch size.")
 parser.add_argument("--debug", default=False, action="store_true", help="If given, run functions eagerly.")
-parser.add_argument("--epochs", default=5, type=int, help="Number of epochs.")
+parser.add_argument("--epochs", default=1, type=int, help="Number of epochs.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
 parser.add_argument("--iou_thr", default=0.5, type=float, help="IoU threshold for gold classes.")
-parser.add_argument("--cls_balancing", default=False, action="store_true", help="Focal loss class balancing")
+parser.add_argument("--cls_balancing", default=True, action="store_true", help="Focal loss class balancing")
 parser.add_argument("--alpha", default=0.25, type=float, help="Focal loss parameter")
 parser.add_argument("--gamma", default=2, type=float, help="Focal loss parameter")
 parser.add_argument("--mask_loss", default=False, action='store_true', help="Set Huber loss to 0 for negative samples")
 
+parser.add_argument("--eval", default=False, action="store_true", help="Load model and only annotate test data")
+parser.add_argument("--model", default="logs/svhn_competition.py-2023-03-26_112804-a=0.25,a=,bs=32,cb=False,d=False,e=1,e=False,g=2,it=0.5,ml=False,m=,s=42,t=1/ep1.h5", type=str, help="Model path")
+parser.add_argument("--anchors", default="logs/svhn_competition.py-2023-03-26_112804-a=0.25,a=,bs=32,cb=False,d=False,e=1,e=False,g=2,it=0.5,ml=False,m=,s=42,t=1/anchors.npy", type=str, help="Anchors path")
 
 # Team members:
 # 4c2c10df-00be-4008-8e01-1526b9225726
@@ -45,9 +48,9 @@ class DetMuchNet(tf.keras.Model):
         p = self._pyramid_network(c)
         
         cls_out = self._classification_head(p)
-        print(cls_out.shape)
+        #print(cls_out.shape)
         box_out = self._regression_head(p)
-        print(box_out.shape)
+        #print(box_out.shape)
 
         outputs = {
             "classes": cls_out,
@@ -127,31 +130,46 @@ class DetMuchNet(tf.keras.Model):
         return box_out       
     
 
-def prepare_examples(img, cls, bbx):
-    paddings = tf.constant([[0, input_shape[0] - img.shape[0]], [0, input_shape[1] - img.shape[1]], [0, 0]])
-    img = tf.pad(img, paddings, mode='constant')
+class AnchorMaster:
+    def __init__(self):
+        anchors = np.array([], dtype=np.float32).reshape(0, 4)
+        for scale in pyramid_scales:
+            scaled_anchors = scale * anchor_shapes
+            for anchor in scaled_anchors:
+                x_positions = np.arange(input_shape[0]/scale)
+                y_positions = np.arange(input_shape[1]/scale)
+                xv, yv = np.meshgrid(x_positions, y_positions)
+                upper_left = np.concatenate([xv[..., np.newaxis], yv[..., np.newaxis]], 2).reshape(-1, 2)
+                upper_left *= scale
+                lower_right = upper_left + anchor[np.newaxis, :]
+                anchor_coords = np.hstack([upper_left, lower_right])
+                anchors = np.vstack([anchors, anchor_coords])
+        self.anchors = anchors
 
-    anchors = np.array([], dtype=np.float32).reshape(0, 4)
-    for scale in pyramid_scales:
-        scaled_anchors = scale * anchor_shapes
-        for anchor in scaled_anchors:
-            x_positions = np.arange(input_shape[0]/scale)
-            y_positions = np.arange(input_shape[1]/scale)
-            xv, yv = np.meshgrid(x_positions, y_positions)
-            upper_left = np.concatenate([xv[..., np.newaxis], yv[..., np.newaxis]], 2).reshape(-1, 2)
-            upper_left *= scale
-            lower_right = upper_left + anchor[np.newaxis, :]
-            anchor_coords = np.hstack([upper_left, lower_right])
-            #print(anchor_coords)
-            anchors = np.vstack([anchors, anchor_coords])
+    def save_anchors(self, dir):
+        np.save(os.path.join(dir, "anchors.npy"), self.anchors)
+
+    def load_anchors(self, path):
+        self.anchors = np.load(path)
+
+    def prepare_examples(self, img, cls, bbx):
+        paddings = tf.constant([[0, input_shape[0] - img.shape[0]], [0, input_shape[1] - img.shape[1]], [0, 0]])
+        img = tf.pad(img, paddings, mode='constant')
+
+        anchor_cls, anchor_bbx = bboxes_utils.bboxes_training(self.anchors, cls.numpy(), bbx.numpy(), args.iou_thr)
+        if args.mask_loss:
+            anchor_bbx = np.hstack((anchor_bbx, tf.expand_dims(anchor_cls, axis=-1)))
+        anchor_cls = tf.one_hot(anchor_cls, 10)
+
+        return img, anchor_cls, anchor_bbx
     
-    #print(anchors)
-    anchor_cls, anchor_bbx = bboxes_utils.bboxes_training(anchors, cls.numpy(), bbx.numpy(), args.iou_thr)
-    if args.mask_loss:
-        anchor_bbx = np.hstack((anchor_bbx, tf.expand_dims(anchor_cls, axis=-1)))
-    anchor_cls = tf.one_hot(anchor_cls, 10)
-
-    return img, anchor_cls, anchor_bbx
+    def prepare_test_examples(self, img):
+        paddings = tf.constant([[0, input_shape[0] - img.shape[0]], [0, input_shape[1] - img.shape[1]], [0, 0]])
+        img = tf.pad(img, paddings, mode='constant')
+        return img
+    
+    def reconstruct_bboxes(self, bbx):
+        return bboxes_utils.bboxes_from_fast_rcnn(self.anchors, bbx)
 
 
 def masked_huber_loss(y_true, y_pred):
@@ -176,76 +194,87 @@ def main(args: argparse.Namespace) -> None:
         datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
         ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", k), v) for k, v in sorted(vars(args).items())))
     ))
+    os.makedirs(args.logdir, exist_ok=True)
 
     # Load the data
     svhn = SVHN()
+    am = AnchorMaster()
 
-    train = svhn.train
-    dev = svhn.dev
-    test = svhn.test
+    if not args.eval:
+        train = svhn.train
+        dev = svhn.dev
+        
+        train = train.map(lambda x: (x["image"], x["classes"], x['bboxes']))
+        train = train.map(lambda img, cls, bbx: tf.py_function(am.prepare_examples, inp=[img, cls, bbx], Tout=[tf.uint8, tf.float32, tf.float32]))
+        train = train.map(lambda img, cls, bbx: (img, {"classes": cls, "boxes": bbx}))
+
+        dev = dev.map(lambda x: (x["image"], x["classes"], x['bboxes']))
+        dev = dev.map(lambda img, cls, bbx: tf.py_function(am.prepare_examples, inp=[img, cls, bbx], Tout=[tf.uint8, tf.float32, tf.float32]))
+        dev = dev.map(lambda img, cls, bbx: (img, {"classes": cls, "boxes": bbx}))
+
+        train = train.shuffle(args.seed)
+        train = train.batch(args.batch_size)
+        train = train.prefetch(tf.data.AUTOTUNE)
+
+        dev = dev.batch(args.batch_size)
+        dev = dev.prefetch(tf.data.AUTOTUNE)
+
+        am.save_anchors(args.logdir)
     
-    train = train.map(lambda x: (x["image"], x["classes"], x['bboxes']))
-    train = train.map(lambda img, cls, bbx: tf.py_function(prepare_examples, inp=[img, cls, bbx], Tout=[tf.uint8, tf.float32, tf.float32]))
-    train = train.map(lambda img, cls, bbx: (img, {"classes": cls, "boxes": bbx}))
+        #for img, lbl in train:
+        #    print(img.shape, lbl["classes"].shape, lbl["boxes"].shape)
 
-    dev = dev.map(lambda x: (x["image"], x["classes"], x['bboxes']))
-    dev = dev.map(lambda img, cls, bbx: tf.py_function(prepare_examples, inp=[img, cls, bbx], Tout=[tf.uint8, tf.float32, tf.float32]))
-    dev = dev.map(lambda img, cls, bbx: (img, {"classes": cls, "boxes": bbx}))
+        # Create the model and train it
+        model = DetMuchNet()
+        if args.mask_loss:
+            loss = masked_huber_loss
+        else:
+            loss = tf.keras.losses.Huber()
+        model.compile(
+            optimizer=tf.optimizers.experimental.AdamW(jit_compile=False),
+            loss={
+                "classes": tf.keras.losses.BinaryFocalCrossentropy(args.cls_balancing, args.alpha, args.gamma),
+                "boxes": loss,
+                #"boxes": tf.keras.losses.Huber(),
+                #"boxes": tf.keras.losses.MeanSquaredError(),
+            },
+            metrics={
+                "classes": [tf.metrics.CategoricalAccuracy("accuracy")],
+                #"boxes": [tf.metrics.BinaryAccuracy("accuracy")],
+            },
+        )
 
-    train = train.shuffle(args.seed)
-    train = train.batch(args.batch_size)
-    train = train.prefetch(tf.data.AUTOTUNE)
+        tb_callback = tf.keras.callbacks.TensorBoard(args.logdir)
+        def save_model(epoch, logs):
+            model.save(os.path.join(args.logdir, f"ep{epoch+1}.h5"), include_optimizer=False)
 
-    dev = dev.batch(args.batch_size)
-    dev = dev.prefetch(tf.data.AUTOTUNE)
-   
-    #for img, lbl in train:
-    #    print(img.shape, lbl["classes"].shape, lbl["boxes"].shape)
-
-    # TODO: Create the model and train it
-    model = DetMuchNet()
-    if args.mask_loss:
-        loss = masked_huber_loss
+        with tf.device("/GPU:0"):
+            model.fit(train, epochs=args.epochs, validation_data=dev, callbacks=[tb_callback, tf.keras.callbacks.LambdaCallback(on_epoch_end=save_model)])
+    
     else:
-        loss = tf.keras.losses.Huber()
-    model.compile(
-        optimizer=tf.optimizers.experimental.AdamW(jit_compile=False),
-        loss={
-            "classes": tf.keras.losses.BinaryFocalCrossentropy(args.cls_balancing, args.alpha, args.gamma),
-            "boxes": loss,
-            #"boxes": tf.keras.losses.Huber(),
-            #"boxes": tf.keras.losses.MeanSquaredError(),
-        },
-        metrics={
-            "classes": [tf.metrics.CategoricalAccuracy("accuracy")],
-            #"boxes": [tf.metrics.BinaryAccuracy("accuracy")],
-        },
-    )
+        model = DetMuchNet()
+        model.load_weights(args.model)
+        am.load_anchors(args.anchors)
 
-    tb_callback = tf.keras.callbacks.TensorBoard(args.logdir)
-    def save_model(epoch, logs):
-        model.save(os.path.join(args.logdir, f"ep{epoch+1}.h5"), include_optimizer=False)
-
-    with tf.device("/GPU:0"):
-        model.fit(train, epochs=args.epochs, validation_data=dev, callbacks=[tb_callback, tf.keras.callbacks.LambdaCallback(on_epoch_end=save_model)])
-
-    exit()
-
-    # TODO
+    test = svhn.test
     test = test.map(lambda x: x["image"])
+    test = test.map(lambda img: tf.py_function(am.prepare_test_examples, inp=[img], Tout=[tf.uint8]))
     test = test.batch(args.batch_size)
 
     # Generate test set annotations, but in `args.logdir` to allow parallel execution.
-    os.makedirs(args.logdir, exist_ok=True)
     with open(os.path.join(args.logdir, "svhn_competition.txt"), "w", encoding="utf-8") as predictions_file:
         # TODO: Predict the digits and their bounding boxes on the test set.
         # Assume that for a single test image we get
         # - `predicted_classes`: a 1D array with the predicted digits,
         # - `predicted_bboxes`: a [len(predicted_classes), 4] array with bboxes;
-        for predicted_classes, predicted_bboxes in ...:
+        out = model.predict(test)
+        for predicted_classes, predicted_bboxes in zip(out['classes'], out['boxes']):
+            predicted_bboxes = am.reconstruct_bboxes(predicted_bboxes)
+            # TODO: Apply nonmax supression
             output = []
             for label, bbox in zip(predicted_classes, predicted_bboxes):
                 output += [label] + list(bbox)
+                break # DELETE DIS PLS
             print(*output, file=predictions_file)
 
 
