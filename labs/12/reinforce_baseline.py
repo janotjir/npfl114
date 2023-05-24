51703176d37354f2c5a51b37ad758ce0d8dcc497
Jiri Janota <janotjir@fel.cvut.cz>
2023-05-24 18:44:24 +0200

    added solution to reinforce_baseline

--- labs/12/reinforce_baseline.py
+++ labs/12/reinforce_baseline.py
@@ -23,10 +23,10 @@ parser.add_argument("--render_each", default=0, type=int, help="Render some epis
 parser.add_argument("--seed", default=None, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
 # For these and any other arguments you add, ReCodEx will keep your default value.
-parser.add_argument("--batch_size", default=..., type=int, help="Batch size.")
-parser.add_argument("--episodes", default=..., type=int, help="Training episodes.")
-parser.add_argument("--hidden_layer_size", default=..., type=int, help="Size of hidden layer.")
-parser.add_argument("--learning_rate", default=..., type=float, help="Learning rate.")
+parser.add_argument("--batch_size", default=1, type=int, help="Batch size.")
+parser.add_argument("--episodes", default=200, type=int, help="Training episodes.")
+parser.add_argument("--hidden_layer_size", default=128, type=int, help="Size of hidden layer.")
+parser.add_argument("--learning_rate", default=0.01, type=float, help="Learning rate.")
 
 
 class Agent:
@@ -41,7 +41,34 @@ class Agent:
         #
         # Using Adam optimizer with given `args.learning_rate` for both models
         # is a good default.
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        '''input = tf.keras.layers.Input(shape=(4,))
+        common = tf.keras.layers.Dense(args.hidden_layer_size, activation=None)(input)
+        
+        action_out = tf.keras.layers.Activation('tanh')(common)
+        action_out = tf.keras.layers.Dense(2, activation=tf.nn.softmax)(action_out)
+        self._model = tf.keras.Model(inputs=input, outputs=action_out)
+
+        baseline_out = tf.keras.layers.Activation('relu')(common)
+        baseline_out = tf.keras.layers.Dense(1, activation=None)(baseline_out)
+        self._model_baseline = tf.keras.Model(inputs=input, outputs=baseline_out)'''
+
+        self._model = tf.keras.Sequential()
+        self._model.add(tf.keras.layers.Input(shape=(4,)))
+        self._model.add(tf.keras.layers.Dense(args.hidden_layer_size, activation='tanh', input_shape=[4]))
+        self._model.add(tf.keras.layers.Dense(2, activation=tf.nn.softmax))
+        
+
+        self._model_baseline = tf.keras.Sequential()
+        self._model_baseline.add(tf.keras.layers.Input(shape=(4,)))
+        self._model_baseline.add(tf.keras.layers.Dense(128, activation='relu', input_shape=[4]))
+        self._model_baseline.add(tf.keras.layers.Dense(1, activation=None))
+
+        self._model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate), 
+                            loss=tf.keras.losses.SparseCategoricalCrossentropy())
+
+        self._model_baseline.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate),
+                                    loss=tf.keras.losses.MeanSquaredError())
 
     # Define a training method.
     #
@@ -56,7 +83,20 @@ class Agent:
         # - train the baseline model to predict `returns`
         # - train the policy model, using `returns - predicted_baseline` as
         #   the advantage estimate
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        with tf.GradientTape() as tape:
+            predicted_baseline = tf.squeeze(tf.cast(self._model_baseline(states), tf.float64))
+            baseline_loss = self._model_baseline.compiled_loss(y_true=returns, y_pred=predicted_baseline)
+        grads_baseline = tape.gradient(baseline_loss, self._model_baseline.trainable_variables)
+        self._model_baseline.optimizer.apply_gradients(zip(grads_baseline, self._model_baseline.trainable_variables))
+
+        with tf.GradientTape() as tape:
+            preds = self._model(states)
+            weights = returns- predicted_baseline
+            loss = self._model.compiled_loss(y_true=actions, y_pred=preds, sample_weight=weights)
+        grads = tape.gradient(loss, self._model.trainable_variables)
+        self._model.optimizer.apply_gradients(zip(grads, self._model.trainable_variables))
+        
 
     # Predict method, again with the `raw_tf_function` for efficiency.
     @wrappers.raw_tf_function(dynamic_dims=1)
@@ -88,7 +128,8 @@ def main(env: wrappers.EvaluationEnv, args: argparse.Namespace) -> None:
                 # TODO(reinforce): Choose `action` according to probabilities
                 # distribution (see `np.random.choice`), which you
                 # can compute using `agent.predict` and current `state`.
-                action = ...
+                probabilities = agent.predict(tf.expand_dims(state, axis=0))[0]
+                action = np.random.choice(2, p=probabilities)
 
                 next_state, reward, terminated, truncated, _ = env.step(action)
                 done = terminated or truncated
@@ -100,17 +141,23 @@ def main(env: wrappers.EvaluationEnv, args: argparse.Namespace) -> None:
                 state = next_state
 
             # TODO(reinforce): Compute returns from the received rewards
+            returns = [np.sum(rewards[i:]) for i in range(len(rewards))]
 
             # TODO(reinforce): Add states, actions and returns to the training batch
+            batch_states.extend(states)
+            batch_actions.extend(actions)
+            batch_returns.extend(returns)
 
         # TODO(reinforce): Train using the generated batch.
+        agent.train(batch_states, batch_actions, batch_returns)
 
     # Final evaluation
     while True:
         state, done = env.reset(start_evaluation=True)[0], False
         while not done:
             # TODO(reinforce): Choose a greedy action
-            action = ...
+            action = agent.predict(tf.expand_dims(state, axis=0))[0]
+            action = tf.math.argmax(action).numpy()
             state, reward, terminated, truncated, _ = env.step(action)
             done = terminated or truncated
 
